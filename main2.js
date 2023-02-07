f664c826334ac6ee52b801f110502a2fb9970b93
Chienmc <101686816+Chienmc@users.noreply.github.com>
2023-02-07 10:30:45 +0700

    Add files via upload

--- main2.js
+++ main2.js
@@ -3,13 +3,13 @@ var app = express()
 
 app.get('/',function(req,res){
     res.writeHead(200,{'Content-Type':'text/html'});
-    res.write('<html><body><p>This is my home Page</p></body></html>');
+    res.write('<html><body><p>This is my home Page</p><a href="/student">Go to Student</a></body></html>');
     res.end();
 })
 
 app.get('/student',function(req,res){
     res.writeHead(200,{'Content-Type':'text/html'});
-    res.write('<html><body><p>This is the student page</p></body></html>');
+    res.write('<html><body><p>This is the student page</p><a href="/">Back to Home</a></body></html>');
     res.end();
 })
 
